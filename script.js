4253b57d9759c351160da46bf77126b491de8a81
John Manouse <jmanouse@mac.com>
2021-03-16 15:27:37 -0400

    set scores to 0

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+//two ways to select an element id
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+
+//set player scores to zero
+score0El.textContent = 0;
+score1El.textContent = 0;
+
